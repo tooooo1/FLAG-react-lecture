6ca5f4d4848ca13aac8ffc736021b88562009d6f
lhjbg0821 <82081355+lhjbg0821@users.noreply.github.com>
2022-12-12 17:34:43 +0900

    3주차 실습
    3주차 실습

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-
+  
+const city = ["서울", "경기", "인천","제주"];
 const First = () => {
+  const [place, setPlace] = useState("서울");
+  const navigate = useNavigate();
+  const onChangeHandler = (e) => {
+    setPlace(e.currentTarget.value);
+  }
   return (
     <Wrapper>
       <div>위치 그림</div>
-      <div>한 눈에 확인하는</div>
-      <div>랭킹먼지</div>
-      <div>미세먼지 농도가 궁금한 지역은?</div>
-      <div>서울</div>
-      <div>검색</div>
+      <SubTitle>한 눈에 확인하는</SubTitle>
+      <Title>랭킹먼지</Title>
+      <Text>미세먼지 농도가 궁금한 지역은?</Text>
+      <select value={place} onChange={onChangeHandler}>
+        {city.map((v)=> 
+            <option value={v}>{v}</option>
+        )}
+      </select>
+      
+      <button onClick={() => navigate('/second', {state: place})}>검색</button>
     </Wrapper>
   );
 };
@@ -22,4 +35,21 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   background-color: rgb(83, 202, 242);
+  color : #FFFFFF;
+`;
+
+const Title = styled.div`
+  font-size:13vw;
+  padding-bottom: 1.5rem;
+  text-align: center;
+  `;
+
+const SubTitle = styled.div`
+  font-size:2rem;
+  padding-bottom: 1.3rem;
+`;
+
+const Text = styled.div`
+  font-size:2rem;
+  padding-bottom: 1.3rem;
 `;
--- src/pages/Second.jsx
+++ src/pages/Second.jsx
@@ -1,9 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
+import Card from "../components/Card";
 
 const Second = () => {
+  const [data, setData] = useState([]);
+  const location = useLocation();
+  const locationCity = location.state;
+
+  useEffect (() => {
+    axios("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.data)
+    .then((json) => setData(json));
+  }, []);
+  
+
   return (
     <Wrapper>
-      <div>위치 그림</div>
+      <div>{locationCity}</div>
+      {data.map((v) => (
+          <Card id="1" title="hi" />
+        ))}
     </Wrapper>
   );
 };
